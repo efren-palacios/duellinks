2d1289f3607ea188d94ee25548d8cf7a91f99fd0
MichielDP <maikel_chat@hotmail.com>
2017-10-11 00:23:03 +0200

    Adjusted topdecks.json to have decktypes as parents with decks as children + DeckType buttons now generate from topdecks.json too + Added NEW on decktypes with new decks

--- js/topdecks.js
+++ js/topdecks.js
@@ -8,14 +8,36 @@ function InitializeViewModel()
 {
     TopDecksViewModel =
     {
-        allDecks: [],
+        allDeckTypes: ko.observableArray(),
         filteredDecks: ko.observableArray(),
 
-        filterByType : function(viewModel, event)
+        filterByType : function(decktype)
         {
-            var type = $(event.currentTarget).attr("data-type");
-            var newDecks = $(TopDecksViewModel.allDecks).filter(function(){return this.decktype === type});
+            var newDecks = $.map($(TopDecksViewModel.allDeckTypes()).filter(function(){return this.id === decktype.id}), function(value, index){ return value.decks });
+            
+            if(newDecks.length !== 0)
+                newDecks.sort(SortDecksByDate);
+                
             TopDecksViewModel.filteredDecks(newDecks);
+        },
+
+        deckTypeHasNewDecks: function(deckType)
+        {
+            var deckTypeHasNewDecks = false;
+
+            $.each(deckType.decks, function(index, deck)
+            {
+                var creationDate = new Date(deck.created);
+                creationDate.setDate(creationDate.getDate() + 3);
+
+                if (creationDate >= new Date())
+                {
+                    deckTypeHasNewDecks = true;
+                    return false;
+                }
+            });
+
+            return deckTypeHasNewDecks;
         }
     };
     
@@ -26,7 +48,22 @@ function GetTopDecks()
 {
     $.getJSON( "/data/topdecks.json", function(data)
     {
-        TopDecksViewModel.filteredDecks(data);
-        TopDecksViewModel.allDecks = data;
+        var defaultDecks = [];
+
+        $.each(data,  function(index, decktype)
+        {
+            $.merge(defaultDecks, decktype.decks);
+        });
+
+        defaultDecks.sort(SortDecksByDate);
+
+        TopDecksViewModel.filteredDecks(defaultDecks);
+
+        TopDecksViewModel.allDeckTypes(data);
     });
 }
+
+function SortDecksByDate(a, b)
+{
+    return ((a.created < b.created) ? 1 : ((a.created > b.created) ? -1 : 0));
+  }
