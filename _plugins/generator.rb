d91190dc8404622b1a2427f9999798216c5912d8
MichielDP <maikel_chat@hotmail.com>
2017-12-09 17:13:00 +0100

    Deck Processing

--- _plugins/generator.rb
+++ _plugins/generator.rb
@@ -1,60 +1,7 @@
 require 'fileutils'
 require 'jekyll'
-require 'json'
 
 module Jekyll
-  class DeckFileGenerator < Jekyll::Generator
-    safe true
-
-    def generate(site)
-      year = Date.today.year.to_s
-      month = Date.today.month.to_s
-
-      pending = site.data["top-decks"]["pending"]
-
-      for file_key in pending.keys
-
-        file = pending[file_key]
-        deck_type = file["deckType"]
-
-        directory = site.source + "/_data/top-decks/" + year + "/" + month + "/" + deck_type + "/"
-        file_name = file["author"].downcase + ".json"
-        full_path = directory + file_name
-
-        #unless File.directory?(directory)
-          #FileUtils.mkdir_p(directory)
-        #end
-
-        #unless File.exist?(full_path)
-          #new_file = File.new(full_path, "w")
-
-          file_main = [file["main0"], file["main1"], file["main2"], file["main3"], file["main4"], file["main5"]]
-          deck_main = []
-
-          for card in file_main
-            deck_main.push({ "name" => card, "amount" => 1})
-          end
-
-          deck =
-          {
-            "name" => file["name"],
-            "author" => file["author"],
-            "created" => file["date"],
-            "skill" => file["skill"],
-            "main" => deck_main,
-            "notes" => file["notes"]
-          }
-
-          #new_file.puts(deck.to_json)
-          #new_file.close
-
-
-        #end
-      end
-    end
-  end
-  
-  
   class DeckPage < Jekyll::Page
   end
 
@@ -62,8 +9,10 @@ module Jekyll
     safe true
 
     def generate(site)
-      site_data = site.data
-      top_decks = site_data["top-decks"]
+
+      # Deck Page Generation
+
+      top_decks = site.data["top-decks"]
 
       for year_key in top_decks.keys - ["pending"]
         year = top_decks[year_key]
@@ -79,7 +28,6 @@ module Jekyll
               unless File.exist?('deck.html')
                 deck_page = File.new('deck.html', 'w')
               end
-                
               
               deck_page.puts("---")
               deck_page.puts("layout: blog")
@@ -123,6 +71,9 @@ module Jekyll
           end
         end
       end
+
+      # End Deck Page Generation
+
     end
   end
 end
