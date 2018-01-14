dcf47dcd5aa43c330bb23cc6fb1a290e811d5c3d
MichielDP <maikel_chat@hotmail.com>
2018-01-14 22:31:28 +0100

    Finished skills stats

--- js/general.js
+++ js/general.js
@@ -1,11 +1,37 @@
 $(document).ready(function()
 {
+    ExecuteOnReadyFunctions();
     BindPageEvents();
 });
 
+function ExecuteOnReadyFunctions()
+{
+    ReadUrlHashFragment();
+}
+
 function BindPageEvents()
 {
+    BindTabsToUrlHash();
     BindCollapsableTables();
+}
+
+function ReadUrlHashFragment()
+{
+    var hash = window.location.hash;
+    
+    if (hash !== "")
+    {
+        $(".nav-tabs a[href='" + hash + "']").tab("show");
+    } 
+}
+
+function BindTabsToUrlHash()
+{
+    $(document).on("shown.bs.tab", "a[data-toggle='tab']", function(event)
+    {
+        window.location.hash = event.target.hash;
+        $(".page-wrapper").scrollTop(0);
+    });
 }
 
 function BindCollapsableTables()
