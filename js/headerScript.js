f32970eb7f8c540556b74a6242429f7d66175bd9
MichielDP <maikel_chat@hotmail.com>
2018-04-11 23:31:51 +0200

    A Cell fix and Header Script file change

--- js/headerScript.js
+++
@@ -1,7 +0,0 @@
-// This file was created specifically to handle variable that 
-// may need to be loaded on runtime, therefore being stored in
-// head of the file
-
-UPLOADCARE_LOCALE = "en";
-UPLOADCARE_TABS = "file";
-UPLOADCARE_PUBLIC_KEY = "8a0cf8abdd31cb4c0348";
