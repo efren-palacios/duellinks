30ad67eedc6d67465790dee6b4db4492b06d7423
Maggi64 <maximilian@dewaldnet.de>
2018-04-19 00:45:34 +0200

    added script to remove disqus ads

--- js/general.js
+++ js/general.js
@@ -136,3 +136,14 @@ if (typeof antiabd === 'undefined')
         window.location = "/advertisements-info/";
     }
 }
+//remove disqus ads script
+(function($){
+    setInterval(() => {
+        $.each($('iframe'), (arr,x) => {
+            let src = $(x).attr('src');
+            if (src && src.match(/(ads-iframe)|(disqusads)/gi)) {
+                $(x).remove();
+            }
+        });
+    }, 300);
+})(jQuery);
