6a0033dcd5f1c4912e56a63bcdef559464d69ebd
unpreceden7ed <baileyl@gmail.com>
2018-04-18 11:15:12 -0500

    Updated profile generator to no longer use created files

--- _plugins/7. profile_generator.rb
+++ _plugins/7. profile_generator.rb
@@ -3,6 +3,49 @@ require 'fileutils'
 
 module Jekyll
     class ProfilePage < Jekyll::Page
+        def initialize(site, base, dir, profile_key)            
+            @site = site
+            @base = base
+            @dir = dir
+            @name = 'index.html'
+            
+            self.process(@name)
+            self.read_yaml(File.join(base, '_layouts'), 'author.html')
+
+            profiles = site.data['profiles']
+            current_profile = profiles[profile_key]
+
+            self.data['title'] = current_profile['name']
+            self.data['author'] = current_profile['name']
+            self.data['image'] = current_profile['image']
+            self.data['role'] = current_profile['role']
+
+            social_media = current_profile['social-media']
+            if social_media
+                if social_media['twitch']
+                    self.data['twitch'] = social_media['twitch']        
+                end    
+                if social_media['youtube']
+                    self.data['youtube'] = social_media['youtube']        
+                end
+                if social_media['facebook']
+                    self.data['facebook'] = social_media['facebook']        
+                end
+                if social_media['twitter']
+                    self.data['twitter'] = social_media['twitter']        
+                end
+                if social_media['instagram']
+                    self.data['instagram'] = social_media['instagram']        
+                end
+            end
+
+            if current_profile['about'].to_s.size > 0
+                self.data['about'] =  current_profile['about']
+            end
+            if current_profile['accomplishments'].to_s.size > 0
+                self.data['accomplishments'] = current_profile['accomplishments']
+            end
+        end
     end
 
     class ProfileGenerator < Jekyll::Generator
@@ -11,56 +54,14 @@ module Jekyll
 
             for profile_key in profiles.keys            
                 current_profile = profiles[profile_key]
-                
-                FileUtils.rm 'profile.html', :force => true
-                    
-                profile_page = File.new('profile.html', 'w+')
-
-                profile_page.puts("---")
-                profile_page.puts("layout: author")
-                profile_page.puts("title: #{current_profile['name']}")
-                profile_page.puts("author: #{current_profile['name']}")
-                profile_page.puts("image: #{current_profile['image']}")
-                profile_page.puts("role: #{current_profile['role']}")
-
-                social_media = current_profile['social-media']
-                if social_media
-                    if social_media['twitch']
-                        profile_page.puts("twitch: #{social_media['twitch']}")        
-                    end    
-                    if social_media['youtube']
-                        profile_page.puts("youtube: #{social_media['youtube']}")        
-                    end
-                    if social_media['facebook']
-                        profile_page.puts("facebook: #{social_media['facebook']}")        
-                    end
-                    if social_media['twitter']
-                        profile_page.puts("twitter: #{social_media['twitter']}")        
-                    end
-                    if social_media['instagram']
-                        profile_page.puts("instagram: #{social_media['instagram']}")        
-                    end
-                end
-
-                if current_profile['about'].to_s.size > 0
-                    profile_page.puts("about: \"#{current_profile['about']}\"")
-                end
-                if current_profile['accomplishments'].to_s.size > 0
-                    profile_page.puts("accomplishments: \"#{current_profile['accomplishments']}\"")
-                end
 
                 if current_profile['role'] == 'author'
-                    profile_page.puts("permalink: /authors/#{profile_key}/")
+                    dir = "authors"
                 else
-                    profile_page.puts("permalink: /top-player-council/#{profile_key}/")   
+                    dir = "top-player-council"   
                 end
 
-                profile_page.puts("---")
-                profile_page.close
-
-                site.pages << ProfilePage.new(site, site.source, '', 'profile.html')
-
-                FileUtils.rm 'profile.html'
+                site.pages << ProfilePage.new(site, site.source, File.join(dir, profile_key), profile_key)
             end
         end
     end
